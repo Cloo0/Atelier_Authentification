8f4905b5b5cdc384bcb5659033dbf6a09b518c4d
Clothilde Delanoy <55195448+Cloo0@users.noreply.github.com>
2025-11-05 10:55:20 +0100

    Fix authToken validation against session token

--- atelier2/index.php
+++ atelier2/index.php
@@ -5,7 +5,7 @@ $_SESSION['token']=== random_bytes(16);
 // Vérifier si l'utilisateur est déjà en possession d'un cookie valide (cookie authToken ayant le contenu 12345)
 // Si l'utilisateur possède déjà ce cookie, il sera redirigé automatiquement vers la page home.php
 // Dans le cas contraire il devra s'identifier.
-if (isset($_COOKIE['authToken']) && $_COOKIE['authToken'] === $username) {
+if (isset($_COOKIE['authToken']) && $_COOKIE['authToken'] === $_SESSION['token']) {
     header('Location: page_admin.php');
     exit();
 }
